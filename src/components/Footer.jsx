0bb0183e7942621ea715a251924ae6377ed33cc2
Pratham choudhary <prathamchoudhary27037@gmail.com>
2020-12-21 13:37:46 +0530

    first phase

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const React = require("react")
+
+function Footer(){
+    return <footer>
+        <p>
+        copyright © {new Date().getFullYear()}
+    </p>
+    </footer>  
+}
+
+export default Footer;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+//6. Make sure that the final website is styled like the example shown here:
+//https://l1pp6.csb.app/
+
+const React = require("react")
+const ReactDOM = require("react-dom")
+import App from "./components/App"
+
+ReactDOM.render(<App />,document.getElementById("root"));
