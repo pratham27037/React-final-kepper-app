572fab6c7bd9bb64af14232d0dcb91475f81f53d
Pratham choudhary <prathamchoudhary27037@gmail.com>
2021-01-02 21:53:04 +0530

    second one

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,15 @@ const React = require("react")
 import Header from "./Header"
 import Footer from "./Footer"
 import Note from "./Note"
-
+import notes from "../notes"
 
 function App(){
     return (
         <div>
            <Header />
-           <Note />
+           {notes.map( note=>{
+            return <Note key={note.id} title={note.title} content={note.content}/> 
+           })}
            <Footer />
         </div>
     )
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,9 +1,9 @@
 const React = require("react")
 
-function Note(){
+function Note(props){
     return <div className="note">
-        <h1>This is the Note Title</h1>
-        <p>This is the Note Content</p>
+        <h1>{props.title}</h1>
+        <p>{props.content}</p>
     </div>
 }
 
